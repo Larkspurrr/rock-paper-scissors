f699c40a80ed260206ef5150ad00789bca65accc
Jesse <111087667+Larkspurrr@users.noreply.github.com>
2023-10-14 11:54:36 -0600

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,15 +1,24 @@
 import pygame
 from random import choice
 pygame.init()
+pygame.mixer.init()
 
 WIDTH = HEIGHT = 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Rock, Paper, Scissors!")
 
+pygame.mixer.music.load("opal.mp3")
+pygame.mixer.music.play(-1)
+
 CHOICE_SIZE = 60
 
 CPU_CHOICES = ["rock", "paper", "scissors"]
 CPU_CHOICE = choice(CPU_CHOICES)
+
+ROCK_IMG = pygame.transform.scale(pygame.image.load("rock.png"), (60, 60))
+PAPER_IMG = pygame.transform.scale(pygame.image.load("paper.png"), (60, 60))
+SCISSORS_IMG = pygame.transform.scale(pygame.image.load("scissors.png"), (60, 60))
+PLAYER_IMG = pygame.transform.scale(pygame.image.load("arrow.png"), (60, 60))
 
 FONT = pygame.font.SysFont("ariel", 40)
 TITLE = FONT.render("Rock, Paper, Scissors!", 1, "black")
@@ -42,10 +51,14 @@ def draw(rock, paper, scissors, player):
 
 	stats()
 
-	pygame.draw.rect(WIN, "darkgray", rock)
-	pygame.draw.rect(WIN, "white", paper)
-	pygame.draw.rect(WIN, "red", scissors)
-	pygame.draw.rect(WIN, "black", player)
+	pygame.draw.rect(WIN, "teal", rock)
+	WIN.blit(ROCK_IMG, rock)
+	pygame.draw.rect(WIN, "teal", paper)
+	WIN.blit(PAPER_IMG, paper)
+	pygame.draw.rect(WIN, "teal", scissors)
+	WIN.blit(SCISSORS_IMG, scissors)
+	pygame.draw.rect(WIN, "teal", player)
+	WIN.blit(PLAYER_IMG, player)
 	pygame.display.flip()
 
 
@@ -56,88 +69,95 @@ def calc_results(rock, paper, scissors):
 	global ties
 	global W_L_COUNT
 
+	WIN.fill("teal")
+
 	# <-- Player Choice Rock Situations -->
 	if player.y == 220:
-		WIN.fill("teal")
 		rock.x, rock.y = RES_POS
-		pygame.draw.rect(WIN, "darkgray", rock)
+		pygame.draw.rect(WIN, "teal", rock)
+		WIN.blit(ROCK_IMG, rock)
 
 		if CPU_CHOICE == "rock":
 			ties += 1
 			cpu_rock.x, cpu_rock.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "darkgray", cpu_rock)
+			pygame.draw.rect(WIN, (0, 0, 0, 0), cpu_rock)
+			WIN.blit(ROCK_IMG, cpu_rock)
 			WIN.blit(TIE_TEXT, (WIDTH/2 - TIE_TEXT.get_width()/2, HEIGHT/3 - TIE_TEXT.get_height()/3))
 
 		elif CPU_CHOICE == "paper":
 			losses += 1
 			cpu_paper.x, cpu_paper.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "white", cpu_paper)
+			pygame.draw.rect(WIN, "teal", cpu_paper)
+			WIN.blit(PAPER_IMG, cpu_paper)
 			WIN.blit(LOST_TEXT, (WIDTH/2 - LOST_TEXT.get_width()/2, HEIGHT/3 - LOST_TEXT.get_height()/3))
 
 		else:
 			wins += 1
 			cpu_scissors.x, cpu_scissors.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "red", cpu_scissors)
+			pygame.draw.rect(WIN, "teal", cpu_scissors)
+			WIN.blit(SCISSORS_IMG, cpu_scissors)
 			WIN.blit(WIN_TEXT, (WIDTH/2 - WIN_TEXT.get_width()/2, HEIGHT/3 - WIN_TEXT.get_height()/3))
 
-		stats()
-		pygame.display.flip()
 	# <-- End of Player Choice Rock Situations -->
 
 	# <-- Player Choice Paper Situations -->
 	elif player.y == 420:
-		WIN.fill("teal")
 		paper.x, paper.y = RES_POS
-		pygame.draw.rect(WIN, "white", paper)
+		pygame.draw.rect(WIN, "teal", paper)
+		WIN.blit(PAPER_IMG, paper)
 
 		if CPU_CHOICE == "rock":
 			wins += 1
 			cpu_rock.x, cpu_rock.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "darkgray", cpu_rock)
+			pygame.draw.rect(WIN, "teal", cpu_rock)
+			WIN.blit(ROCK_IMG, cpu_rock)
 			WIN.blit(WIN_TEXT, (WIDTH/2 - WIN_TEXT.get_width()/2, HEIGHT/3 - WIN_TEXT.get_height()/3))
 
 		elif CPU_CHOICE == "paper":
 			ties += 1
 			cpu_paper.x, cpu_paper.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "white", cpu_paper)
+			pygame.draw.rect(WIN, "teal", cpu_paper)
+			WIN.blit(PAPER_IMG, cpu_paper)
 			WIN.blit(TIE_TEXT, (WIDTH/2 - TIE_TEXT.get_width()/2, HEIGHT/3 - TIE_TEXT.get_height()/3))
 
 		else:
 			losses += 1
 			cpu_scissors.x, cpu_scissors.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "red", cpu_scissors)
+			pygame.draw.rect(WIN, "teal", cpu_scissors)
+			WIN.blit(SCISSORS_IMG, cpu_scissors)
 			WIN.blit(LOST_TEXT, (WIDTH/2 - LOST_TEXT.get_width()/2, HEIGHT/3 - LOST_TEXT.get_height()/3))
 
-		stats()
-		pygame.display.flip()
 	# <-- End of Player Choice Paper Situations -->
 
 	# <-- Player Choice Scissors Situations -->
 	elif player.y == 620:
-		WIN.fill("teal")
 		scissors.x, scissors.y = RES_POS
-		pygame.draw.rect(WIN, "red", scissors)
+		pygame.draw.rect(WIN, "teal", scissors)
+		WIN.blit(SCISSORS_IMG, scissors)
 
 		if CPU_CHOICE == "rock":
 			losses += 1
 			cpu_rock.x, cpu_rock.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "darkgray", cpu_rock)
+			pygame.draw.rect(WIN, "teal", cpu_rock)
+			WIN.blit(ROCK_IMG, cpu_rock)
 			WIN.blit(LOST_TEXT, (WIDTH/2 - LOST_TEXT.get_width()/2, HEIGHT/3 - LOST_TEXT.get_height()/3))
 			
 		elif CPU_CHOICE == "paper":
 			wins += 1
 			cpu_paper.x, cpu_paper.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "white", cpu_paper)
+			pygame.draw.rect(WIN, "teal", cpu_paper)
+			WIN.blit(PAPER_IMG, cpu_paper)
 			WIN.blit(WIN_TEXT, (WIDTH/2 - WIN_TEXT.get_width()/2, HEIGHT/3 - WIN_TEXT.get_height()/3))
 
 		else:
 			ties += 1
 			cpu_scissors.x, cpu_scissors.y = RES_CPU_POS
-			pygame.draw.rect(WIN, "red", cpu_scissors)
+			pygame.draw.rect(WIN, "teal", cpu_scissors)
+			WIN.blit(SCISSORS_IMG, cpu_scissors)
 			WIN.blit(TIE_TEXT, (WIDTH/2 - TIE_TEXT.get_width()/2, HEIGHT/3 - TIE_TEXT.get_height()/3))
-
-		stats()
-		pygame.display.flip()
+	
+	stats()
+	pygame.display.flip()
 	# <-- End of Player Choice Scissors Situations -->
 
 
@@ -184,7 +204,7 @@ while run:
 				else:
 					x = player.y
 					while player.y != x - 400:
-						player.y -= PLAYER_SPEED
+						player.y -= PLAYER_SPEED * 2
 						draw(rock, paper, scissors, player)
 			elif (keys[pygame.K_UP] or keys[pygame.K_w]) and (can_move == True):
 				if player.y != 220:
@@ -195,12 +215,11 @@ while run:
 				else:
 					x = player.y
 					while player.y != x + 400:
-						player.y += PLAYER_SPEED
+						player.y += PLAYER_SPEED * 2
 						draw(rock, paper, scissors, player)
 			if keys[pygame.K_r] and can_move == False:
 				can_move = True
 				draw(rock, paper, scissors, player)
 				CPU_CHOICE = choice(CPU_CHOICES)
-
 
 pygame.quit()
